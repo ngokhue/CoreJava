5366e2e74f1cba02be0e0409df36ca197c7cfcf2
ngokhue <ngokhue@users.noreply.github.com>
2019-07-30 22:55:30 +0700

    update lai code theo comment cua thay

--- src/test/java/collection/ListFootBallTest.java
+++ src/test/java/collection/ListFootBallTest.java
@@ -5,48 +5,55 @@ import java.util.Collections;
 import java.util.List;
 
 public class ListFootBallTest {
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 //		1.1
-		List<String> footballTeam = new ArrayList<String>();
-		footballTeam.add("Tottenham Hotspur");
-		footballTeam.add("Paris Saint Germain");
-		footballTeam.add("Arsenal");
-		footballTeam.add("Chelsea");
-		footballTeam.add("Manchester United");
-		footballTeam.add("Liverpool");
-		footballTeam.add("Manchester City");
-		footballTeam.add("Bayern Munich");
-		footballTeam.add("Barcelona");
+		List<String> footballTeams = new ArrayList<String>();
+		footballTeams.add("Tottenham Hotspur");
+		footballTeams.add("Paris Saint Germain");
+		footballTeams.add("Arsenal");
+		footballTeams.add("Chelsea");
+		footballTeams.add("Manchester United");
+		footballTeams.add("Liverpool");
+		footballTeams.add("Manchester City");
+		footballTeams.add("Bayern Munich");
+		footballTeams.add("Barcelona");
 
-		System.out.println("Các đội bóng gồm: \t");
-		for(int i=0; i<footballTeam.size();i++) {
-			System.out.println(i+1+". "+footballTeam.get(i));
-		}		
-		System.out.println("\t");		
-		Boolean check = false;	
-		for(int i=0;i<footballTeam.size();i++) {
-			if(footballTeam.get(i)=="Manchester United")
+		System.out.println("Các đội bóng gồm:");
+		int size = footballTeams.size();
+		for (int i = 0; i < size; i++) {
+			System.out.println(i + 1 + ". " + footballTeams.get(i));
+		}
+
+//		C1
+		Boolean check = false;
+		for (int i = 0; i < size; i++) {
+//			if(footballTeams.get(i)=="Manchester United")
+			if ("Manchester United".equals(footballTeams.get(i)))
 				check = true;
 		}
-		if(check==true)
-			System.out.println("Wowww \t");
-		else 
-			System.out.println("!!!!!!!! \t");
+		System.out.println((check=true)?"Wowwwwwww": "!!!!!!!!!!!!!!");
 		
-		Boolean exist = footballTeam.contains("Manchester United");		
+//		C2
+		for (String team : footballTeams) {
+            String str = team.equals("Manchester United") ? "Wowwwww" : "!!!!!!!";
+            System.out.println(str);
+        }
+
+//		C3
+		Boolean exist = footballTeams.contains("Manchester United");
 		System.out.println(((exist = true) ? "Wowww" : "!!!!!"));
-		
-		
+
 //		1.2		
-		String[] values = {"Dean Henderson","Victor Lindelof","Chris Smalling","Marcos Rojo"};	
+		String[] values = { "Dean Henderson", "Victor Lindelof", "Chris Smalling", "Marcos Rojo" };
 		ArrayList<String> footballPlayer = new ArrayList<String>();
 		Collections.addAll(footballPlayer, values);
 		System.out.println(footballPlayer);
-		
+
 		ArrayList<String> list = new ArrayList<String>();
-		for(int i=0;i<values.length;i++) {
+		int length = values.length;
+		for (int i = 0; i < length; i++) {
 			list.add(values[i]);
-		}	
+		}
 		System.out.println(list);
 	}
 
