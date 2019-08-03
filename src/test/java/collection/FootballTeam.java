6850abbcf5c8dacbfdd13cf91500210c253b97ad
ngokhue <ngokhue@users.noreply.github.com>
2019-08-03 22:59:48 +0700

    Bai 2
    Doc du lieu tu file -> in ra file

--- src/test/java/collection/FootballTeam.java
+++ src/test/java/collection/FootballTeam.java
@@ -1,54 +1,64 @@
 package collection;
-
-import java.util.Arrays;
-import java.util.List;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class FootballTeam {
-	private String club, manager;
+	private String teamName, teamManager;
 
-	public String getClub() {
-	    return club;
+	public String getTeamName() {
+		return teamName;
 	}
 
-	public void setClub(String club) {
-	    this.club = club;
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
 	}
 
-	public String getManager() {
-	    return manager;
+	public String getTeamLeader() {
+		return teamManager;
 	}
 
-	public void setManager(String manager) {
-	    this.manager = manager;
+	public void setTeamLeader(String teamLeader) {
+		this.teamManager = teamLeader;
 	}
 
-	public FootballTeam(String club, String manager) {
-	    this.club = club;
-	    this.manager = manager;
+	public FootballTeam(String teamName, String teamLeader) {
+		this.teamName = teamName;
+		this.teamManager = teamLeader;
 	}
 
 	@Override
 	public String toString() {
-	    return "\nClub: " + club + ", Manager: " + manager;
+		return "Team name = " + teamName + ", team leader = " + teamManager;
 	}
 
+	public static void main(String[] args) throws IOException {
+//		List<FootballTeam> list = Arrays.asList(new FootballTeam("abc", "def"), new FootballTeam("ghk", "dml"));
+//		System.out.println(list);
+//		list.stream().filter(a -> ((FootballTeam) a).getTeamLeader().startsWith("d"))
+//				.forEach(b -> System.out.println("s11111111"));
 
-	public static void main(String[] args) {
-	    List<FootballTeam> list = Arrays.asList(
-	            new FootballTeam("Manchester", "Ole Gunnar Solskjær"),
-	            new FootballTeam("Chelsea", "Frank Lampard"),
-	            new FootballTeam("Liverpool", "John The Reds"),
-	            new FootballTeam("HAGL", "John The Rock"),
-	            new FootballTeam("Arsenal", "Unai Emery")
-	    );
+		String input = " FootballClubs.txt";
+		ArrayList<FootballTeam> footballTeams = new ArrayList<FootballTeam>();
 
-	    System.out.println(list);
-	    System.out.println("-------------");
+		try (Stream<String> stream = Files.lines(Paths.get(input), StandardCharsets.UTF_8)) {
+			stream.filter(a -> a.substring(a.indexOf("\t") + 1).startsWith("John")).forEach(line -> {
+				footballTeams.add(new FootballTeam(line.substring(0, line.indexOf("\t")),
+						line.substring(line.indexOf("\t") + 1)));
+			});
 
-	    list.stream()
-	            .filter(ele -> ele.getManager().startsWith("John"))
-	            .forEach(s -> System.out.println("Club has John: " + s.getClub()));
-	    
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+
+		FileOutputStream fo = new FileOutputStream("FootballJohn.txt");
+        PrintWriter out = new PrintWriter(fo);
+        out.print(footballTeams);
+        out.close();		
 	}
-
 }
